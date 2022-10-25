5ef63b687d1196c0b4cf56c7d3e65d92b75be3ea
pppppplk <58656331+pppppplk@users.noreply.github.com>
2022-10-25 21:44:38 +0300

    skills models & schemas
    Реализованы схемы и модели для навыков.
    Решили отказаться от уровней.
    В следующей итерации реализуем crud.

--- src/app/api/__init__.py
+++ src/app/api/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from app.api import projects, career, employees
+from app.api import projects, employees, career
 
 router = APIRouter(prefix="/v1", responses={404: {"description": "Not found"}})
 
 router.include_router(projects.router)
-router.include_router(career.skills_router)
 router.include_router(employees.router)
+router.include_router(career.skills_router)
