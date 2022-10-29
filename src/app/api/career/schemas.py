070707557205f348082685662bd1701387a60eb1
Alexey Kovalev <41exey.kov41ev@gmail.com>
2022-10-29 04:50:56 +0300

    Feature/resources roles models (#31)
    * Pre-commit config added
    
    * [Github Actions] Code quality checks config added
    
    * startup project with docker small guid
    
    * [Github Actions] Fix python version 3.10
    
    * [Github Actions] Ignore migrations in pylint
    
    * Black reformated
    
    * Black reformat just in src dir
    
    * Dockerfile fixed & ports changed
    
    * README.md добавил
    
    * improvement of guidelines
    
    * env configuration enhancements
    
    * removed extra line break
    
    * [README] Pre-commit description added
    
    * [README] Pre-commit description added v2
    
    * Base models added
    
    Co-authored-by: Demka <16972045+GeorgiyDemo@users.noreply.github.com>
    Co-authored-by: ruki <rukizhat01@mail.ru>

--- src/app/api/career/schemas.py
+++ src/app/api/career/schemas.py
@@ -1,6 +1,46 @@
 from app.core.schemas import BaseSchema
 
 
+class RolesBase(BaseSchema):
+    name: str
+
+
+class RolesCreate(RolesBase):
+    pass
+
+
+class RolesUpdate(RolesBase):
+    name: str | None = None
+
+
+class RolesRead(RolesBase):
+    id: int
+
+
+class GradesBase(BaseSchema):
+    name: str
+
+
+class GradesCreate(GradesBase):
+    pass
+
+
+class GradesUpdate(GradesBase):
+    name: str | None = None
+
+
+class GradesRead(GradesBase):
+    id: int
+
+
+class GradesSchema(GradesBase):
+    authors: list[RolesBase]
+
+
+class RolesSchema(RolesBase):
+    books: list[GradesBase]
+
+
 class SkillsBase(BaseSchema):
     name: str
     max_score: int
